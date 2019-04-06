b4074352e0fdf6571c50722949085e2eda7d5f8c
LenweSaralonde <lenwe@lenwe.info>
2019-04-06 20:43:41 +0200

    Demo songs are added to the list on a fresh new installation

--- core/MusicianList.lua
+++ core/MusicianList.lua
@@ -24,6 +24,7 @@ function MusicianList:OnInitialize()
 			['version'] = MusicianList.STORAGE_VERSION,
 			['data'] = {}
 		}
+		MusicianList.RestoreDemoSongs()
 	end
 
 	-- Check for outdated Musician version
