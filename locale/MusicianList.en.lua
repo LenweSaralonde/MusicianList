cd2d0223d04aad8bee9e996af582515df87e74f9
LenweSaralonde <lenwe@lenwe.info>
2021-05-02 17:31:47 +0200

    Added localization guide

--- locale/MusicianList.en.lua
+++ locale/MusicianList.en.lua
@@ -1,4 +1,20 @@
+------------------------------------------------------------------------
+-- Please read the localization guide in the Wiki:
+-- https://github.com/LenweSaralonde/Musician/wiki/Localization
+--
+-- * Commented out msg lines need to be translated.
+-- * Do not translate anything on the left hand side of the = sign.
+-- * Do not translate placeholders in curly braces ({variable}).
+-- * Keep the text as a single line. Use \n for carriage return.
+-- * Escape double quotes (") with a backslash (\").
+-- * Check the result in game to make sure your text fits the UI.
+------------------------------------------------------------------------
+
 local msg = MusicianList.InitLocale('en', "English", 'enUS', 'enGB')
+
+------------------------------------------------------------------------
+---------------- ↑↑↑ DO NOT EDIT THE LINES ABOVE ! ↑↑↑  ----------------
+------------------------------------------------------------------------
 
 --- Chat commands
 msg.COMMAND_SAVE = "Save the current song in the list"
