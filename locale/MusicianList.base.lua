b84e1c37a3727a985e7fa090fd0f22d3da63c81c
LenweSaralonde <lenwe@lenwe.info>
2021-05-18 19:08:26 +0200

    Added localization team in credits

--- locale/MusicianList.base.lua
+++ locale/MusicianList.base.lua
@@ -1,2 +1,17 @@
 MusicianList.LocaleBase = {}
 local msg = MusicianList.LocaleBase
+
+if Musician.LocalizationTeam then
+	tAppendAll(Musician.LocalizationTeam, {
+		{ 'de', Musician.DefaultTranslator },
+		{ 'en', "LenweSaralonde" },
+		{ 'es', Musician.DefaultTranslator },
+		{ 'fr', "LenweSaralonde" },
+		{ 'it', Musician.DefaultTranslator },
+		{ 'ko', Musician.DefaultTranslator },
+		{ 'pt', Musician.DefaultTranslator },
+		{ 'ru', Musician.DefaultTranslator, "Hubbotu" },
+		{ 'tw', Musician.DefaultTranslator },
+		{ 'zh', "Grayson Blackclaw" },
+	})
+end
